cf31c9e677e4b019d98f5d7e70437a98a903b8c0
ab405h <ab405h@att.com>
2016-10-27 17:00:26 -0500

    Added a mockup using Material Design Lite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AnameComponent } from './components/aname/aname.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/aname/aname.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aname',
+  templateUrl: './aname.component.html',
+  styleUrls: ['./aname.component.css']
+})
+export class AnameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
