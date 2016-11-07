a881530f8112ff1c8d3d1104c2592fb790260d3d
Nhat Dao <daoanhnhat1995@gmail.com>
2016-11-07 01:25:53 -0600

    done mongo

--- express/app.js
+++ express/app.js
@@ -1,7 +1,6 @@
 
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+var path = require('path'); var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -33,25 +32,25 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // production error handler
 // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 db.connect('mongodb://daonhat:nhatdao@ds147167.mlab.com:47167/somethingcool', function(err) {
   if (err) {
@@ -60,9 +59,8 @@ db.connect('mongodb://daonhat:nhatdao@ds147167.mlab.com:47167/somethingcool', fu
   } else {
 	  console.log('db connected');
 
-    app.listen(3000, function() {
-      console.log('Listening on port 3000...')
-    });
   }
 });
+
+module.exports = app;
 
